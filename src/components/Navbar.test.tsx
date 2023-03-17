6d96e7e8f016f42aa4afaaf73bcfe4cbed56c8d8
Isac Petinate <idpetinate@gmail.com>
2023-03-17 14:38:54 -0300

    Implementing unit tests

--- src/components/Navbar.test.tsx
+++ src/components/Navbar.test.tsx
@@ -1,10 +1,11 @@
-import { render, screen } from '@testing-library/react'
+import { customRender, renderHook, screen, waitFor, user } from '../../test'
+
+import { useSearchStore } from '@/store/useSearchStore'
 
 import { Navbar } from '@/components/Navbar'
-import { useRouter } from 'next/router'
 
 describe('Navbar', () => {
-    const renderComponent = () => render(<Navbar />)
+    const renderComponent = () => customRender(<Navbar />)
 
     test('Should render properly', async () => {
         renderComponent()
@@ -14,6 +15,50 @@ describe('Navbar', () => {
         const searchInput = await screen.findByPlaceholderText('Pesquisar')
 
         expect(link).toBeInTheDocument()
+        expect(logo).toBeInTheDocument()
         expect(searchInput).toBeInTheDocument()
+    })
+
+    test('Should render fill search input', async () => {
+        renderComponent()
+
+        const searchInput = await screen.findByPlaceholderText('Pesquisar')
+
+        expect(searchInput).toBeInTheDocument()
+        expect(searchInput).toHaveValue('')
+
+        await user.type(searchInput, 'linux')
+
+        expect(searchInput).toHaveValue('linux')
+    })
+
+    test('Should render fill search input and store', async () => {
+        renderComponent()
+
+        const {
+            result: { current }
+        } = renderHook(() => useSearchStore())
+
+        const setTermSpy = await jest.spyOn(current, 'setTerm')
+        const searchInput = await screen.findByPlaceholderText('Pesquisar')
+
+        await user.type(searchInput, 'linux')
+
+        waitFor(() => expect(setTermSpy).toHaveBeenCalled())
+
+        expect(searchInput).toHaveValue('linux')
+        waitFor(() => expect(current.term).toHaveValue('linux'))
+    })
+
+    test('Should go to home when click on logo link', async () => {
+        const push = jest.fn()
+
+        customRender(<Navbar />, { router: { push } })
+
+        const link = await screen.findByRole('link', { name: /GHub/i })
+
+        await user.click(link)
+
+        expect(push).toHaveBeenCalled()
     })
 })
