ed50008b2776a0446339013314d97dbc569536dc
Isac Petinate <idpetinate@gmail.com>
2023-03-17 14:38:17 -0300

    Refactor to improve UX

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -23,26 +23,39 @@ export function RepoCard({ repo }: RepoCardProps) {
 
     let { styles, attributes } = usePopper(referenceElement, popperElement)
 
+    const handleOpenRepoPage = useCallback(() => {
+        if (!window) return
+
+        window?.open(repo.html_url, '_blank')
+    }, [repo.html_url])
+
     return (
         <div className='w-80 h-80 border rounded flex flex-col justify-between overflow-hidden'>
             <div className='flex flex-row justify-between'>
                 <div className='flex flex-row items-center gap-3 p-3'>
-                    <ArchiveBoxIcon className='w-10 h-10' />
+                    <ArchiveBoxIcon
+                        className='w-10 h-10'
+                        data-testid='repo-card-icon'
+                    />
+
                     <div className='w-full overflow-hidden'>
                         <h2 className='text-xl'>{repo.name}</h2>
 
-                        <p className='text-sm truncate text-ellipsis overflow-hidden w-48'>
-                            <Tooltip content={repo.description}>
+                        <Tooltip content={repo.description}>
+                            <p className='text-sm truncate text-ellipsis overflow-hidden w-48'>
                                 {repo.description ?? 'Sem descrição'}
-                            </Tooltip>
-                        </p>
+                            </p>
+                        </Tooltip>
                     </div>
                 </div>
 
                 <div className='h-full flex items-center px-2'>
-                    <a target='_blank' href={repo.html_url}>
+                    <button
+                        onClick={handleOpenRepoPage}
+                        aria-label='Open repository page on GitHub in a new page'
+                    >
                         <ChevronRightIcon className='w-8 h-8' />
-                    </a>
+                    </button>
                 </div>
             </div>
 
@@ -111,7 +124,7 @@ export function RepoCard({ repo }: RepoCardProps) {
 
                     <div className='px-3 py-2 flex flex-row justify-between items-center'>
                         <Avatar
-                            img={user?.avatar_url}
+                            img={user?.avatar_url ?? ''}
                             placeholderInitials={user?.name?.charAt(0)}
                         >
                             <div className=' font-medium dark:text-white'>
@@ -128,6 +141,8 @@ export function RepoCard({ repo }: RepoCardProps) {
                                 /* Use the `open` state to conditionally change the direction of the chevron icon. */
                                 <>
                                     <Popover.Button
+                                        name='open-user-info'
+                                        aria-label='Show user info'
                                         ref={setReferenceElement as any}
                                     >
                                         <InformationCircleIcon className='w-6 h-6 text-emerald-500' />
@@ -141,7 +156,7 @@ export function RepoCard({ repo }: RepoCardProps) {
                                     >
                                         <div className='bg-white w-60 p-2 rounded-md flex flex-col justify-start items-start gap-2 z-50'>
                                             <Avatar
-                                                img={user?.avatar_url}
+                                                img={user?.avatar_url ?? ''}
                                                 placeholderInitials={user?.name?.charAt(
                                                     0
                                                 )}
@@ -196,7 +211,7 @@ export function RepoCard({ repo }: RepoCardProps) {
                                             ) : null}
 
                                             <a
-                                                href={user?.html_url}
+                                                href={user?.html_url ?? ''}
                                                 target='_blank'
                                                 className='h-5 w-full text-center bg-violet-300 text-sm rounded text-slate-900'
                                             >
