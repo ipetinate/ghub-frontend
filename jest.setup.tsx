393d7f72daca1fe4a184f351a8b18f901e9a68b3
Isac Petinate <idpetinate@gmail.com>
2023-03-17 14:36:24 -0300

    Remove next/router mock

--- jest.setup.tsx
+++ jest.setup.tsx
@@ -24,21 +24,3 @@ jest.mock('next/image', () => ({
         return <img {...props} />
     }
 }))
-
-jest.mock('next/router', () => ({
-    useRouter() {
-        return {
-            route: '/',
-            pathname: '',
-            query: '',
-            asPath: '',
-            push: jest.fn(),
-            events: {
-                on: jest.fn(),
-                off: jest.fn()
-            },
-            beforePopState: jest.fn(() => null),
-            prefetch: jest.fn(() => null)
-        }
-    }
-}))
