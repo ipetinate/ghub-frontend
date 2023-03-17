93534cd1977582968ff676ac3f14b352b1f25dab
Isac Petinate <idpetinate@gmail.com>
2023-03-17 16:17:17 -0300

    Fix jest deprecated warning: change `tsConfig` to `tsconfig`

--- jest.config.js
+++ jest.config.js
@@ -8,9 +8,9 @@ module.exports = {
         '^.+\\.tsx?$': [
             'ts-jest',
             {
+                diagnostics: true,
                 isolatedModules: true,
-                tsConfig: 'tsconfig.jest.json',
-                diagnostics: true
+                tsconfig: 'tsconfig.jest.json'
             }
         ],
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
